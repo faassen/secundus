cac4ad13c8b3577605ae31ef8d50614467be3409
faassen <faassen@startifact.com>
2016-09-12 14:46:10 +0200

    A few sci fi changes:
    * don't need to say DataFrameContainer; secundus should know to use
      one when it is returned from a component directive.
    
    * add indexes to the data frames. But what should we really do for color
      and texture?
    
    In general, could components be initialized independently from each
    other like this, or is it often easier to initialize them together?

--- experiments/experiment_secundus.py
+++ experiments/experiment_secundus.py
@@ -14,45 +14,52 @@ class App(secundus.App):
 
 @App.component('position')
 def get_position(app):
-    return secundus.DataFrameContainer(pd.DataFrame({
+    return pd.DataFrame({
         'x': np.random.rand(app.amount),
         'y': np.random.rand(app.amount)
-    }))
+    }, index=range(app.amount))
 
 
 @App.component('velocity')
 def get_velocity(app):
-    return secundus.DataFrameContainer(pd.DataFrame({
+    return pd.DataFrame({
         'x_speed': np.random.randn(app.amount) / 10,
         'y_speed': np.random.randn(app.amount) / 10
-    }))
+    }, index=range(app.amount))
 
 
 @App.component('color')
 def get_color(app):
-    return secundus.DataFrameContainer(pd.DataFrame(
-        [255, 255, 255, 255] * app.amount * 4))
+    # XXX the range is incorrect with this structure.
+    return pd.DataFrame(
+        [255, 255, 255, 255] * app.amount * 4,
+        index=range(app.amount))
 
 
 @App.component('texture')
 def get_texture(app):
-    return secundus.DataFrameContainer(pd.DataFrame(
-        app.image.texture.tex_coords * app.amount))
+    # XXX the range is incorrect with this structure
+    return pd.DataFrame(
+        app.image.texture.tex_coords * app.amount,
+        index=range(app.amount))
 
 
 @App.system(['position', 'velocity'])
 def update_position_by_velocity(app, update, entity_ids, position, velocity):
-    position['x'] += velocity['x_speed'] * update.dt
-    position['y'] += velocity['y_speed'] * update.dt
+    position.loc[entity_ids, 'x'] += velocity.loc[
+        entity_ids, 'x_speed'] * update.dt
+    position.loc[entity_ids, 'y'] += velocity[
+        entity_ids, 'y_speed'] * update.dt
 
 
 @App.system(['position', 'color', 'texture'])
 def render(app, update, entity_ids, position, color, texture):
-    scaled = position * 600
+    scaled = position.loc[entity_ids] * 600
     x1 = scaled['x'] - app.image.anchor_x
     y1 = scaled['y'] - app.image.anchor_y
     x2 = x1 + app.image.width
     y2 = y1 + app.image.height
+    # what should the entity ids be here?
     df = pd.DataFrame({'a_x': x1, 'a_y': y1,
                        'b_x': x2, 'b_y': y1,
                        'c_x': x2, 'c_y': y2,
@@ -67,8 +74,8 @@ def render(app, update, entity_ids, position, color, texture):
         GL_ONE_MINUS_SRC_ALPHA)
     batch.add(len(scaled.values) * 4, GL_QUADS, group,
               ('v2f/dynamic', df.values.flatten()),
-              ('c4B', color.values),
-              ('t3f', texture.values))
+              ('c4B', color.values.flatten()),
+              ('t3f', texture.values.flatten()))
     batch.draw()
 
 
